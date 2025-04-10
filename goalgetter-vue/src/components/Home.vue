38db50287d948d63cabb06e275c734e34169387a
jsakoman <josip@sakoman.ch>
2025-04-10 19:56:53 +0200

    implemented app basic structure

---
+++ goalgetter-vue/src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>Goal Getter</h1>
+      <h2>Bertlo Lusetic & Josip Sakoman</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped>
+/* Optional styles */
+</style>
