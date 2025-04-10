581a1fe811333593b5fddc94fd03e9e6fed22da3
jsakoman <josip@sakoman.ch>
2025-04-10 20:19:59 +0200

    adjusted router and home is now start

--- goalgetter-vue/src/components/Home.vue
+++ goalgetter-vue/src/components/Home.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="home">
     <h1>Goal Getter</h1>
-      <h2>Bertlo Lusetic & Josip Sakoman</h2>
+    <h2>Bertlo Lusetic & Josip Sakoman</h2>
+    <router-link to="/income">Go to Income Form</router-link>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home'
+  name: 'Home',
 }
 </script>
 
--- goalgetter-vue/src/router.js
+++ goalgetter-vue/src/router.js
@@ -1,17 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from './components/Home.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from './components/Home.vue';
+import IncomeForm from './components/IncomeForm.vue';
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
-  }
-]
+    component: Home,
+  },
+  {
+    path: '/income',
+    name: 'IncomeForm',
+    component: IncomeForm,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
