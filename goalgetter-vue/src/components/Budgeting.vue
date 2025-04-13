e5f62bf32200532a501de889935528418ef9266b
jsakoman <josip@sakoman.ch>
2025-04-13 12:59:34 +0200

    added expenseForm and small improvements in styles.css

--- goalgetter-vue/src/components/Budgeting.vue
+++ goalgetter-vue/src/components/Budgeting.vue
@@ -1,50 +1,74 @@
 <template>
   <div class="container">
-    <div class="total-income-container">
-      <!-- Display Total Income -->
-      <div class="total-income">
-        <h3>Total Income: {{ totalIncome }} CHF</h3>
+
+    <!-- INCOMES -->
+    <div class="tab">
+      <div class="total-container">
+        <!-- Clickable Total Income -->
+        <div class="total-amount clickable" @click="showIncomeForm = !showIncomeForm">
+          <h3>Total Income: {{ totalIncome }} CHF</h3>
+        </div>
       </div>
 
-      <!-- Button to toggle Income Form on the right -->
-      <button @click="showIncomeForm = !showIncomeForm" class="toggle-button">
-        {{ showIncomeForm ? 'Hide Income Form' : 'Incomes' }}
-      </button>
+      <!-- Income Form -->
+      <IncomeForm v-if="showIncomeForm" />
     </div>
 
-    <!-- Income Form -->
-    <IncomeForm v-if="showIncomeForm" />
+    <!-- EXPENSES -->
+    <div class="tab">
+      <div class="total-container">
+        <!-- Clickable Total Expense -->
+        <div class="total-amount clickable" @click="showExpenseForm = !showExpenseForm">
+          <h3>Total Expenses: {{ totalExpense }} CHF</h3>
+        </div>
+      </div>
+
+      <!-- Expense Form -->
+      <ExpenseForm v-if="showExpenseForm" />
+    </div>
+
   </div>
 </template>
 
 <script>
 import IncomeForm from './Budgeting/IncomeForm.vue';
+import ExpenseForm from './Budgeting/ExpenseForm.vue';
 import { fetchAll } from '../services/crudService';
 
 export default {
   name: 'Budgeting',
   components: {
-    IncomeForm
+    IncomeForm,
+    ExpenseForm
   },
   data() {
     return {
       showIncomeForm: false, // Default not showing the form
-      incomes: [] // Income data will be fetched here
+      incomes: [], // Income data will be fetched here
+      showExpenseForm: false, // Default not showing the form
+      expenses: [] // Expense data will be fetched here
     };
   },
   computed: {
-    // Compute the total income by summing all the income amounts
+    // Compute the total amount by summing all the items amounts
     totalIncome() {
       return this.incomes.reduce((sum, income) => sum + income.amount, 0);
+    },
+    totalExpense() {
+      return this.expenses.reduce((sum, expense) => sum + expense.amount, 0);
     }
   },
   methods: {
     async loadIncomes() {
       this.incomes = await fetchAll('incomes');
+    },
+    async loadExpenses() {
+      this.expenses = await fetchAll('expenses');
     }
   },
   mounted() {
-    this.loadIncomes();
+    this.loadIncomes(),
+    this.loadExpenses();
   }
 };
 </script>
--- goalgetter-vue/src/components/Budgeting/ExpenseForm.vue
+++ goalgetter-vue/src/components/Budgeting/ExpenseForm.vue
@@ -1,35 +1,35 @@
 <template>
-  <div class="table-container"> 
-    <!-- Add New Income Form (shown when Add Income button is clicked) -->
+  <div class="table-container">    
+    <!-- Add New Expense Form (shown when Add Expense button is clicked) -->
     <div v-if="showAddForm">
-      <h3 class="form-heading">Add new Income</h3>
-      <input v-model="name" class="table-input" placeholder="Name (e.g., Salary)" />
-      <input v-model.number="amount" class="table-input" type="number" placeholder="Amount (e.g., 4500)" />
-      <button @click="addIncome" class="table-button add-button">Add</button>
+      <h3 class="form-heading">Add new Expense</h3>
+      <input v-model="name" class="table-input" placeholder="Name (e.g. Rent)" />
+      <input v-model.number="amount" class="table-input" type="number" placeholder="Amount (e.g., 1000)" />
+      <button @click="addExpense" class="table-button add-button">Add</button>
       <button @click="cancelAdd" class="table-button cancel-button">Cancel</button>
     </div>
 
-    <!-- Edit Income Form (shown when editing) -->
+    <!-- Edit Expense Form (shown when editing) -->
     <div v-if="editing">
-      <h4 class="form-heading">Edit Income</h4>
+      <h4 class="form-heading">Edit Expense</h4>
       <input v-model="editName" class="table-input" placeholder="Edit Name" />
       <input v-model.number="editAmount" class="table-input" type="number" placeholder="Edit Amount" />
-      <button @click="updateIncome" class="table-button save-button">Save</button>
+      <button @click="updateExpense" class="table-button save-button">Save</button>
       <button @click="cancelEdit" class="table-button cancel-button">Cancel</button>
     </div>
 
-    <!-- List of Incomes -->
-    <h3 class="form-heading">Income List</h3>
+    <!-- List of Expenses -->
+    <h3 class="form-heading">Expense List</h3>
     <ul class="table-list">
-      <li v-for="item in incomes" :key="item.id" class="table-item">
+      <li v-for="item in expenses" :key="item.id" class="table-item">
         {{ item.name }} - {{ item.amount }} CHF
-        <button @click="deleteIncome(item.id)" class="table-button delete-button">Delete</button>
+        <button @click="deleteExpense(item.id)" class="table-button delete-button">Delete</button>
         <button @click="startEditing(item)" class="table-button edit-button">Edit</button>
       </li>
     </ul>
 
-    <!-- "+" Button to show the Add Income Form -->
-    <button @click="toggleAddForm" class="table-button add-button toggle-button">+ (Add new)</button>
+        <!-- Add Expense button -->
+        <button @click="toggleAddForm" class="table-button add-button toggle-button">+ (Add new)</button>
   </div>
 </template>
   
@@ -42,34 +42,34 @@ import {
 } from '../../services/crudService';
 
 export default {
-  name: 'IncomeForm',
+  name: 'ExpenseForm',
   data() {
     return {
       name: '',
       amount: 0,
-      incomes: [],
+      expenses: [],
       editing: false,
       editId: null,
       editName: '',
       editAmount: 0,
-      showAddForm: false // Controls the visibility of Add New Income form
+      showAddForm: false // Controls the visibility of Add New Expense form
     };
   },
   methods: {
-    async loadIncomes() {
-      this.incomes = await fetchAll('incomes');
+    async loadExpenses() {
+      this.expenses = await fetchAll('expenses');
     },
-    async addIncome() {
+    async addExpense() {
       if (!this.name || !this.amount) return;
-      await addToCollection('incomes', { name: this.name, amount: this.amount });
+      await addToCollection('expenses', { name: this.name, amount: this.amount });
       this.name = '';
       this.amount = 0;
       this.showAddForm = false; // Hide the add form after adding
-      this.loadIncomes();
+      this.loadExpenses();
     },
-    async deleteIncome(id) {
-      await deleteFromCollection('incomes', id);
-      this.loadIncomes();
+    async deleteExpense(id) {
+      await deleteFromCollection('expenses', id);
+      this.loadExpenses();
     },
     startEditing(item) {
       this.editing = true;
@@ -78,10 +78,10 @@ export default {
       this.editAmount = item.amount;
       this.showAddForm = false; // Hide the Add form when editing
     },
-    async updateIncome() {
-      await updateInCollection('incomes', this.editId, { name: this.editName, amount: this.editAmount });
+    async updateExpense() {
+      await updateInCollection('expenses', this.editId, { name: this.editName, amount: this.editAmount });
       this.cancelEdit();
-      this.loadIncomes();
+      this.loadExpenses();
     },
     cancelEdit() {
       this.editing = false;
@@ -96,11 +96,11 @@ export default {
     },
     toggleAddForm() {
       this.showAddForm = !this.showAddForm;
-      this.editing = false; // Hide edit form when adding new income
+      this.editing = false; // Hide edit form when adding new expenses
     }
   },
   mounted() {
-    this.loadIncomes();
+    this.loadExpenses();
   }
 };
 </script>
